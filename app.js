1c0de1d7a0fdca42cade564f50b7a2b3cfe2b2dd
tazbin <mdtazbinur@gmail.com>
2024-11-02 17:21:40 +0600

    Add semicolon

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ import {sum} from './math/index.js';
 
 app.use(morgan('combined'));
 
-const port = 3000
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!' + sum(10, 20))
+  res.send('Hello World!' + sum(10, 20));
 });
 
 app.get('/health', (req, res) => {
