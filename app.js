9d5d36049c1c8468b744b51ba9a9bdcbd26abcca
tazbin <mdtazbinur@gmail.com>
2024-12-04 08:58:10 +0600

    sum added

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!!!');
+  res.send('Hello World!!!' + sum(10, 202));
 });
 
 app.get('/health', (req, res) => {
